cea5b369aff34f4087506860a4ab976bd2fd5b65
Filip Holec <fholec@engeto.com>
2022-07-21 15:48:11 +0200

    Add file with arithmetic tests

---
+++ files/arithmetic.js
@@ -0,0 +1,18 @@
+let a = 10;
+let b = 7;
+let c = 2;
+
+console.log(a + b);
+console.log(a + b + c);
+
+console.log(a - b);
+console.log(b - a);
+
+console.log(a * b);
+console.log(a ** b);
+
+console.log(a / b);
+console.log(a % b);
+
+console.log(4 + 5 - 10 + 7 * 4 + 3)
+console.log((4 + 5 - 10 + 7) * 4 + 3)
