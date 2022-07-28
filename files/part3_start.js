2c6d10e3cb19af9eaddb1f9b18876133c6933d22
Filip Holec <fholec@engeto.com>
2022-07-28 16:24:11 +0200

    Update part3_start.js

---
+++ files/part3_start.js
@@ -0,0 +1,46 @@
+// Define variables with let / const
+const a = 10;
+const b = 7;
+let c = 2;
+
+console.log("====================================================");
+console.log("      Let's start with 3rd part of the lesson       ");
+console.log("====================================================");
+
+// console.log("a:", a, "b:", b, "c:", c);
+
+// Arithmetic operations
+
+// Comparison operators
+const x = '10';
+
+// Operator precedence
+// console.log("Operator precedence");
+
+// Comments
+// console.log("====================================================");
+// console.log("                   Comment types                    ");
+// console.log("====================================================");
+
+// Define variable test
+
+// Alternative commentary
+
+/* This is a multiline JavaScript comment
+ * You can use this format in all files and JS scripts you create
+ */
+
+/**
+* Function that greets a user
+* @author   Filip
+* @param    {String} name    Name of the user
+* @return   {String}         Greeting message
+*/
+// function greetUser(name) {
+//     return `Greetings, ${name}!`;
+// }
+
+// Console log types
+// console.log("====================================================");
+// console.log("                 CONSOLE Log types                  ");
+// console.log("====================================================");
