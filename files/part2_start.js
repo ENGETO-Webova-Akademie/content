8c74f38f0ebdaa817c02af316ca188cb63b76563
matej <msveda@engeto.com>
2022-08-01 11:43:01 +0200

    upload part 2 start & end files

---
+++ files/part2_start.js
@@ -0,0 +1,81 @@
+// DEKLARACE PROMENNYCH
+
+// spolecna uloha #1
+console.log('DEKLARACE PROMENNYCH');
+
+
+
+// spolecna uloha #2
+
+
+
+
+// samostatna uloha #1
+// - vytvorte promennou s nazvem `mojeJmeno` (`jmeno uz je obsazena`), priradte ji jako hodnotu sve jmeno
+// - vyvorte promennou `hobby`, priradte ji jako hodnotu nejaky svuj konicek
+// - nezapomenout ze hodnoty v tomto pripade budou text - davame do uvozovek
+// - v console.log vytisknout vetu "Jmenuji se <jmeno> a mam rad(a) <hobby>"
+
+
+
+// VYSKAKOVACI OKNA #1 (prompt)
+// spolecna uloha #3 
+console.log('VYSKAKOVACI OKNA #1');
+
+
+
+// DATOVE TYPY
+console.log('DATOVE TYPY');
+// Number
+console.log('Number');
+
+
+
+// String
+console.log('String');
+
+
+
+// Boolean
+console.log('Boolean');
+
+
+
+// Undefined
+console.log('Undefined');
+
+
+
+// Prevadeni String na Number
+console.log('Prevadeni String na Number');
+
+// spolecna uloha #4
+
+
+
+
+// ------------------------- 
+
+// VYSKAKOVACI OKNA #2
+console.log('VYSKAKOVACI OKNA #2');
+
+// alert
+
+
+
+
+// prompt
+
+
+
+// confirm
+
+
+
+// samostatna uloha #2
+// napiste program, ktery:
+// - vyzve uzivatele, aby napsal sve jmeno, ulozi ho do promenne 'jmeno' 
+// - vyyve uzivatele, aby napsal jak se citi, ulozi ho do promenne 'pocit' 
+// - vyskakovaci okno s textem, ze napr. 'Lenka se citi jako hacker'
+
+
